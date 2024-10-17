503d252057d220c3142900c9d46029e035fba4aa
FinloAnne <annexxx25@gmail.com>
2024-10-17 21:08:47 +0200

    update basic javascript

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#Search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+  let currentCityElement = document.querySelector("#current-city");
+  currentCityElement.innerHTML = searchInput.value;
 }
 
 let searchFormElement = document.querySelector("#Search-form");
